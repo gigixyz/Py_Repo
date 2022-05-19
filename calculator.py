e61d9ce2cc7cec746a9d0905635077270c91de67
gigixyz <gigi.marga99@gmail.com>
2022-05-19 10:50:04 +0300

    Afisarea diferentei dintre cele doua numere

--- calculator.py
+++ calculator.py
@@ -2,3 +2,4 @@ x = input('Introduceti primul numar natural')
 y = input('Introduceti al doilea numar natural')
 
 print('Suma celor doua numere este {}'.format(x + y))
+print('Diferenta dintre cele doua numere este {}'.format(x - y))
